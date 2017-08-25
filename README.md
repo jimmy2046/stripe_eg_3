4915808c51b6377996bda9273a0e7663c38eff4c
jimmy2046 <jimmychong_st@yahoo.com>
2017-08-24 21:35:53 -0700

    Stripe First Draft

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+Using Stripe Checkout and Rails<br />
+Reference: https://stripe.com/docs/checkout/rails
+
+## Revision history:
+* Aug 24, 2017: first draft
---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,21 @@
+class ProductsController < ApplicationController
+
+  def show
+    @product = Product.find(params[:id])
+  end    
+    
+ def new
+ end
+
+def create
+  @product = Product.new(product_params)
+ 
+  @product.save
+  redirect_to @product
+end
+ 
+private
+  def product_params
+    params.require(:product).permit(:description, :price)
+  end
+end
