b0b0e4c335b9e75ffa4101c482371a31be1fcd7c
jimmy2046 <jimmychong_st@yahoo.com>
2017-08-25 12:02:39 -0700

    Updated public key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => "pk_test_xA9xhaprB5bmoSGhuldyMoZI",
+  :secret_key      => "sk_test_2EGH2rntaIxsQJaXEJGBtMdp"
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
